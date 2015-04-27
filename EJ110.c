3b121acdd3d975104e72f66907e48bb996338735
prottowalter <protto.walter@yahoo.com>
2015-04-27 00:53:39 -0300

    Modificacion y agregado del archivo generador

--- EJ110.c
+++ EJ110.c
@@ -1,6 +1,6 @@
 /* saca por la salida la entrada estandar con los tabuladores, espacios, y barras invertidas visibilizadas
    Protto, Walter Fabián
-   15/04/2015 */
+   27/04/2015 */
   
 #include<stdio.h>
 
@@ -9,17 +9,20 @@ main (void)
 int c;
 
 while ((c=getchar())!=EOF)
-	if ((c!='\t')&&(c!='\\')&&(c!=' '))
+	if ((c!='\t')&&(c!='\\')&&(c!='\b'))
 	putchar(c); else
 			if (c=='\t')
 		 		{
 				putchar('\\');
 				putchar('t'); 
 				} else
-					if (c==' ')
-						putchar('_'); else  /*Lo cambie por que dice el \b (backspace)*/
-							{
-							putchar('\\');
-							putchar('\\');
-							}			 				
+				   if (c=='\b')
+				    {
+				    putchar('\\');
+			    	    putchar('b');
+			            } else  
+					  {
+					  putchar('\\');
+					  putchar('\\');
+					  }			 				
 }		
---
+++ archivoEntrada.c
@@ -0,0 +1,10 @@
+/* Es el generador de la entrada del programa
+   Protto, Walter Fabián
+   27/04/2015 */
+  
+#include<stdio.h>
+
+main (void)
+{
+printf("Soy walter\\alumno de\be la:\t\\UTN");
+}
